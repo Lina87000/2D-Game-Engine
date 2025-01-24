bcaba1529595f1d968786ae9c690e2ff81d8b8c0
Lina V <linavaillendet@gmail.com>
2025-01-24 20:58:31 +0100

    Updated rendering engine
    Created Shader  Program and VBO/VAO/EBO handling.

---
+++ src/ShaderClass.cpp
@@ -0,0 +1,63 @@
+#include"headers/ShaderClass.h"
+
+// Reads a text file and outputs a string with everything in the text file
+std::string get_file_contents(const char* filename)
+{
+	std::ifstream in(filename, std::ios::binary);
+	if (in)
+	{
+		std::string contents;
+		in.seekg(0, std::ios::end);
+		contents.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&contents[0], contents.size());
+		in.close();
+		return(contents);
+	}
+	throw std::runtime_error("Failed to open file: " + std::string(filename));
+}
+
+// Constructor that build the Shader Program from 2 different shaders
+Shader::Shader(const char* vertexFile, const char* fragmentFile)
+{
+	// Read vertexFile and fragmentFile and store the strings
+	std::string vertexCode = get_file_contents(vertexFile);
+	std::string fragmentCode = get_file_contents(fragmentFile);
+
+	// Convert the shader source strings into character arrays
+	const char* vertexSource = vertexCode.c_str();
+	const char* fragmentSource = fragmentCode.c_str();
+
+	// Create and compile vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Create and compile vertex shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// Create Shader Program Object and get its reference
+	ID = glCreateProgram();
+	glAttachShader(ID, vertexShader);
+	glAttachShader(ID, fragmentShader);
+	glLinkProgram(ID);
+
+	// Delete vertex and fragment shader
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+}
+
+// Activates the shader program
+void Shader::Activate()
+{
+	glUseProgram(ID);
+}
+
+// Deletes the shader program
+void Shader::Delete()
+{
+	glDeleteProgram(ID);
+}
